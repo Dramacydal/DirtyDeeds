daaf8ef42b4129b3342f36346013d6f4868adbf0
Yaki Khadafi <ElSolDolLo@gmail.com>
2014-07-15 18:32:02 +0300

    first commit

---
+++ Itchy/Itchy.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Itchy
+{
+    partial class Itchy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.attachButton = new System.Windows.Forms.Button();
+            this.installButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // attachButton
+            // 
+            this.attachButton.Location = new System.Drawing.Point(12, 12);
+            this.attachButton.Name = "attachButton";
+            this.attachButton.Size = new System.Drawing.Size(75, 23);
+            this.attachButton.TabIndex = 0;
+            this.attachButton.Text = "Attach";
+            this.attachButton.UseVisualStyleBackColor = true;
+            this.attachButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // installButton
+            // 
+            this.installButton.Location = new System.Drawing.Point(12, 55);
+            this.installButton.Name = "installButton";
+            this.installButton.Size = new System.Drawing.Size(75, 23);
+            this.installButton.TabIndex = 1;
+            this.installButton.Text = "Install";
+            this.installButton.UseVisualStyleBackColor = true;
+            this.installButton.Click += new System.EventHandler(this.installButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(12, 99);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Itchy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.installButton);
+            this.Controls.Add(this.attachButton);
+            this.Name = "Itchy";
+            this.Text = "Itchy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Itchy_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button attachButton;
+        private System.Windows.Forms.Button installButton;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ Itchy/Itchy.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Win32HWBP;
+
+namespace Itchy
+{
+    public partial class Itchy : Form
+    {
+        public Itchy()
+        {
+            InitializeComponent();
+        }
+
+        ProcessDebugger pd = null;
+        Thread th = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pd == null)
+            {
+                if (!WinApi.SetDebugPrivileges())
+                {
+                    MessageBox.Show("Failed to set debug privileges");
+                    return;
+                }
+
+                Process[] list = Process.GetProcessesByName("d2loader-1.12");
+                if (list.Length == 0)
+                    return;
+
+                var process = list[0];
+                pd = new ProcessDebugger(process.Id);
+                th = ProcessDebugger.Run(ref pd);
+
+                MessageBox.Show("Attached");
+            }
+        }
+
+        private void Itchy_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pd != null)
+            {
+                pd.StopDebugging();
+                th.Join();
+            }
+        }
+
+        class LightBreakPoint : HardwareBreakPoint
+        {
+            public LightBreakPoint(int address, uint len, Condition condition) : base(address, len, condition) { }
+
+            public override bool HandleException(ref CONTEXT ctx, ProcessDebugger pd)
+            {
+                ctx.Eax = 0xFF;     // light density
+                ctx.Eip += 0xEB;    // skip code
+
+                return true;
+            }
+        }
+
+        class RainBreakPoint : HardwareBreakPoint
+        {
+            public RainBreakPoint(int address, uint len, Condition condition) : base(address, len, condition) { }
+
+            public override bool HandleException(ref CONTEXT ctx, ProcessDebugger pd)
+            {
+                ctx.Eax &= 0xFFFFFF00;
+                ctx.Eip += 4;
+
+                return true;
+            }
+        }
+
+        private void installButton_Click(object sender, EventArgs e)
+        {
+            if (pd == null)
+                return;
+
+            var bp = new LightBreakPoint(0x5F907, 1, HardwareBreakPoint.Condition.Code);
+            pd.AddBreakPoint("D2Client.dll", bp);
+
+            var bp2 = new RainBreakPoint(0x73A02, 1, HardwareBreakPoint.Condition.Code);
+            pd.AddBreakPoint("d2common.dll", bp2);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (pd == null)
+                return;
+
+            pd.RemoveBreakPoints();
+        }
+    }
+}
