027b6356ddbf84490d9af8754624b912e40aaa3b
Yaki Khadafi <ElSolDolLo@gmail.com>
2014-07-21 11:20:58 +0300

    rewrite to use WhiteMagic instead of BlackMagic

--- Itchy/D2Game.cs
+++ Itchy/D2Game.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Diagnostics;
 using System.Windows.Forms;
 using WhiteMagic;
 
@@ -94,60 +95,38 @@ namespace Itchy
 
         public bool GetPlayerUnit(out UnitAny unit)
         {
-            var addr = pd.BlackMagic.AllocateMemory(1024);
+            var pUnit = pd.MemoryHandler.Call(pd.GetModuleAddress("d2client.dll") + D2Client.GetPlayerUnit,
+                CallingConventionEx.StdCall);
 
-            pd.BlackMagic.Asm.Clear();
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2client.dll") + 0x613C0);
-            pd.BlackMagic.Asm.AddLine("retn");
-            var playerAddr = pd.BlackMagic.Asm.InjectAndExecute(addr);
-            pd.BlackMagic.FreeMemory(addr);
-            if (playerAddr == 0)
+            if (pUnit == 0)
             {
                 unit = new UnitAny();
                 return false;
             }
 
-            unit = (UnitAny)pd.BlackMagic.ReadObject(playerAddr, typeof(UnitAny));
+            unit = pd.MemoryHandler.Read<UnitAny>(pUnit);
             return true;
         }
 
         public void Test()
         {
-            var m = new MemoryHandler(process);
-            m.SuspendAllThreads();
-            var addr2 = pd.BlackMagic.AllocateMemory(1024);
-            if (addr2 == 0)
-                MessageBox.Show("Fail #1");
+            /*var dt = DateTime.Now.Ticks;
 
-            pd.BlackMagic.WriteUnicodeString(addr2, "lol!!");
+            SuspendProcess();
+            for (var i = 0; i < 500; ++i)
+            {
+                UnitAny u;
+                GetPlayerUnit(out u);
+            }
+            ResumeProcess();
 
-            pd.BlackMagic.Asm.AddLine("push 7");
-            pd.BlackMagic.Asm.AddLine("push {0}", addr2);
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2client.dll") + D2Client.PrintGameString);
-            pd.BlackMagic.Asm.AddLine("retn");
-
-            pd.BlackMagic.Asm.InjectAndExecute(addr2 + 50);
-
-            if (!pd.BlackMagic.FreeMemory(addr2))
-                MessageBox.Show("Fail #3");
-
-            m.ResumeAllThreads();
-
-
-
-            return;
-            var addr = pd.BlackMagic.AllocateMemory(1024);
-
-            UnitAny unit;
-            if (GetPlayerUnit(out unit))
-                MessageBox.Show(unit.dwAct.ToString());
-
+            MessageBox.Show(((DateTime.Now.Ticks - dt) / TimeSpan.TicksPerMillisecond).ToString());*/
             RevealAct();
         }
 
         public int[] m_ActLevels = new int[]
         {
-            1, 40, 75, 103, 109, 137
+            1, 10, 75, 103, 109, 137
         };
 
         private uint GetLevelPointer(uint pActMisc, int nLevel)
@@ -155,57 +134,39 @@ namespace Itchy
             if (pActMisc == 0 || nLevel < 0)
                 return 0;
 
-            ActMisc actMist = (ActMisc)pd.BlackMagic.ReadObject(pActMisc, typeof(ActMisc));
+            var actMist = pd.MemoryHandler.Read<ActMisc>(pActMisc);
             Level level;
             for (uint pLevel = actMist.pLevelFirst; pLevel != 0; pLevel = level.pNextLevel)
             {
-                level = (Level)pd.BlackMagic.ReadObject(pLevel, typeof(Level));
+                level = pd.MemoryHandler.Read<Level>(pLevel);
                 if (level.dwLevelNo == nLevel && level.dwSizeX > 0)
                     return pLevel;
             }
 
-            var addr = pd.BlackMagic.AllocateMemory(1024);
-            pd.BlackMagic.Asm.Clear();
-            pd.BlackMagic.Asm.AddLine("mov ecx, {0}", pActMisc);
-            pd.BlackMagic.Asm.AddLine("mov edx, {0}", nLevel);
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2common.dll") + D2Common.GetLevel);
-            pd.BlackMagic.Asm.AddLine("retn");
-
-            var ret = pd.BlackMagic.Asm.InjectAndExecute(addr);
-            pd.BlackMagic.FreeMemory(addr);
-            return ret;
+            return pd.MemoryHandler.Call(pd.GetModuleAddress("d2common.dll") + D2Common.GetLevel,
+                CallingConventionEx.FastCall,
+                pActMisc, (uint)nLevel);
         }
 
         private void InitLevel(uint pLevel)
         {
-            var addr = pd.BlackMagic.AllocateMemory(1024);
-            pd.BlackMagic.Asm.Clear();
-            pd.BlackMagic.Asm.AddLine("push {0}", pLevel);
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2common.dll") + D2Common.InitLevel);
-            pd.BlackMagic.Asm.AddLine("retn");
-            pd.BlackMagic.Asm.InjectAndExecute(addr);
+            pd.MemoryHandler.Call(pd.GetModuleAddress("d2common.dll") + D2Common.InitLevel,
+                CallingConventionEx.StdCall,
+                pLevel);
         }
 
         private uint InitAutomapLayer(uint levelno)
         {
-            var addr = pd.BlackMagic.AllocateMemory(1024);
-            pd.BlackMagic.Asm.Clear();
-            pd.BlackMagic.Asm.AddLine("mov eax, {0}", levelno);
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2common.dll") + D2Common.GetLayer);
-            pd.BlackMagic.Asm.AddLine("retn");
-            var ret = pd.BlackMagic.Asm.InjectAndExecute(addr);
-            pd.BlackMagic.FreeMemory(addr);
+            var ret = pd.MemoryHandler.Call(pd.GetModuleAddress("d2common.dll") + D2Common.GetLayer,
+                CallingConventionEx.FastCall,
+                levelno);
             if (ret == 0)
                 return 0;
 
-            AutomapLayer2 layer = (AutomapLayer2)pd.BlackMagic.ReadObject(ret, typeof(AutomapLayer2));
-            addr = pd.BlackMagic.AllocateMemory(1024);
-            pd.BlackMagic.Asm.Clear();
-            pd.BlackMagic.Asm.AddLine("mov eax, {0}", layer.nLayerNo);
-            pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2client.dll") + D2Client.InitAutomapLayer_I);
-            pd.BlackMagic.Asm.AddLine("retn");
-            ret = pd.BlackMagic.Asm.InjectAndExecute(addr);
-            pd.BlackMagic.FreeMemory(addr);
+            var layer = pd.MemoryHandler.Read<AutomapLayer2>(ret);
+            ret = pd.MemoryHandler.Call(pd.GetModuleAddress("d2client.dll") + D2Client.InitAutomapLayer_I,
+                CallingConventionEx.Register,
+                layer.nLayerNo);
 
             return ret;
         }
@@ -219,76 +180,60 @@ namespace Itchy
             if (!GetPlayerUnit(out unit))
                 return;
 
-            uint pRoom2 = lvl.pRoom2First;
+            var misc = pd.MemoryHandler.Read<ActMisc>(lvl.pMisc);
+            var path = pd.MemoryHandler.Read<Path>(unit.pPath);
+            var pLayer = pd.MemoryHandler.ReadUInt(pd.GetModuleAddress("d2client.dll") + D2Client.pAutoMapLayer);
+
+            var pRoom2 = lvl.pRoom2First;
+            var roomCount = 0;
             for (; ; )
             {
                 if (pRoom2 == 0)
                     break;
 
-                Room2 room2 = (Room2)pd.BlackMagic.ReadObject(pRoom2, typeof(Room2));
-                var added = false;
+                ++roomCount;
 
-                ActMisc misc = new ActMisc();
-                Path path = new Path();
+                var room2 = pd.MemoryHandler.Read<Room2>(pRoom2);
+                var added = false;
+                
                 if (room2.pRoom1 == 0)
                 {
-                    misc = (ActMisc)pd.BlackMagic.ReadObject(lvl.pMisc, typeof(ActMisc));
-                    path = (Path)pd.BlackMagic.ReadObject(unit.pPath, typeof(Path));
-
-                    var addr = pd.BlackMagic.AllocateMemory(1024);
-                    pd.BlackMagic.Asm.Clear();
-                    pd.BlackMagic.Asm.AddLine("push {0}", path.pRoom1);
-                    pd.BlackMagic.Asm.AddLine("push {0}", room2.dwPosY);
-                    pd.BlackMagic.Asm.AddLine("push {0}", room2.dwPosX);
-                    pd.BlackMagic.Asm.AddLine("push {0}", lvl.dwLevelNo);
-                    pd.BlackMagic.Asm.AddLine("push {0}", misc.pAct);
-                    pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2common.dll") + D2Common.AddRoomData);
-                    pd.BlackMagic.Asm.AddLine("retn");
-                    pd.BlackMagic.Asm.InjectAndExecute(addr);
-                    pd.BlackMagic.FreeMemory(addr);
+                    pRoom2 = room2.pRoom2Next;
+                    continue;
+                    pd.MemoryHandler.Call(pd.GetModuleAddress("d2common.dll") + D2Common.AddRoomData,
+                        CallingConventionEx.StdCall,
+                        misc.pAct, lvl.dwLevelNo, room2.dwPosX, room2.dwPosY, path.pRoom1);
 
                     added = true;
+                    room2 = pd.MemoryHandler.Read<Room2>(pRoom2);
                 }
 
-                room2 = (Room2)pd.BlackMagic.ReadObject(pRoom2, typeof(Room2));
                 if (room2.pRoom1 == 0)
+                {
+                    pRoom2 = room2.pRoom2Next;
                     continue;
+                }
 
-                var addr2 = pd.BlackMagic.AllocateMemory(1024);
-                pd.BlackMagic.Asm.Clear();
-                var pLayer = pd.BlackMagic.ReadUInt(pd.GetModuleAddress("d2client.dll") + D2Client.pAutoMapLayer);
-                pd.BlackMagic.Asm.AddLine("push {0}", pLayer);
-                pd.BlackMagic.Asm.AddLine("push {0}", 1);
-                pd.BlackMagic.Asm.AddLine("push {0}", room2.pRoom1);
-                pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2client.dll") + D2Client.RevealAutomapRoom);
-                pd.BlackMagic.Asm.AddLine("retn");
-                pd.BlackMagic.Asm.InjectAndExecute(addr2);
-                pd.BlackMagic.FreeMemory(addr2);
+                pd.MemoryHandler.Call(pd.GetModuleAddress("d2client.dll") + D2Client.RevealAutomapRoom,
+                    CallingConventionEx.StdCall,
+                    room2.pRoom1, 1, pLayer);
 
                 //Drawpresets(...)
 
                 if (added)
                 {
-                    var addr = pd.BlackMagic.AllocateMemory(1024);
-                    pd.BlackMagic.Asm.Clear();
-                    pd.BlackMagic.Asm.AddLine("push {0}", path.pRoom1);
-                    pd.BlackMagic.Asm.AddLine("push {0}", room2.dwPosY);
-                    pd.BlackMagic.Asm.AddLine("push {0}", room2.dwPosX);
-                    pd.BlackMagic.Asm.AddLine("push {0}", lvl.dwLevelNo);
-                    pd.BlackMagic.Asm.AddLine("push {0}", misc.pAct);
-                    pd.BlackMagic.Asm.AddLine("call {0}", pd.GetModuleAddress("d2common.dll") + D2Common.RemoveRoomData);
-                    pd.BlackMagic.Asm.AddLine("retn");
-                    pd.BlackMagic.Asm.InjectAndExecute(addr);
-                    pd.BlackMagic.FreeMemory(addr);
+                    pd.MemoryHandler.Call(pd.GetModuleAddress("d2common.dll") + D2Common.RemoveRoomData,
+                        CallingConventionEx.StdCall,
+                        misc.pAct, lvl.dwLevelNo, room2.dwPosX, room2.dwPosY, path.pRoom1);
                 }
 
                 pRoom2 = room2.pRoom2Next;
             }
 
-            Path path2 = (Path)pd.BlackMagic.ReadObject(unit.pPath, typeof(Path));
-            Room1 room1 = (Room1)pd.BlackMagic.ReadObject(path2.pRoom1, typeof(Room1));
-            Room2 room22 = (Room2)pd.BlackMagic.ReadObject(room1.pRoom2, typeof(Room2));
-            Level lvl2 = (Level)pd.BlackMagic.ReadObject(room22.pLevel, typeof(Level));
+            var path2 = pd.MemoryHandler.Read<Path>(unit.pPath);
+            var room1 = pd.MemoryHandler.Read<Room1>(path2.pRoom1);
+            var room22 = pd.MemoryHandler.Read<Room2>(room1.pRoom2);
+            var lvl2 = pd.MemoryHandler.Read<Level>(room22.pLevel);
 
             InitAutomapLayer(lvl2.dwLevelNo);
         }
@@ -306,23 +251,25 @@ namespace Itchy
                 return;
             }
 
-            Act act = (Act)pd.BlackMagic.ReadObject(unit.pAct, typeof(Act));
+            //var lvl = pd.MemoryHandler.Read<Level>(unit
+
+            var act = pd.MemoryHandler.Read<Act>(unit.pAct);
             for (var i = m_ActLevels[act.dwAct] + 1; i < m_ActLevels[act.dwAct + 1]; ++i)
             {
-                uint pLevel = GetLevelPointer(act.pMisc, i);
+                var pLevel = GetLevelPointer(act.pMisc, i);
                 if (pLevel == 0)
                     continue;
 
-                Level lvl = (Level)pd.BlackMagic.ReadObject(pLevel, typeof(Level));
+                var lvl = pd.MemoryHandler.Read<Level>(pLevel);
                 if (lvl.pRoom2First == 0)
                     InitLevel(pLevel);
-                lvl = (Level)pd.BlackMagic.ReadObject(pLevel, typeof(Level));
+                lvl = pd.MemoryHandler.Read<Level>(pLevel);
                 if (lvl.pRoom2First == 0)
                     continue;
 
                 RevealLevel(lvl);
 
-                MessageBox.Show("Revealing " + i.ToString());
+                //MessageBox.Show("Revealing " + i.ToString());
             }
 
             //MessageBox.Show(str);
@@ -332,38 +279,12 @@ namespace Itchy
 
         private void SuspendProcess()
         {
-            process.Refresh();
-            foreach (ProcessThread pT in process.Threads)
-            {
-                //if (pT.Id == pd.BlackMagic.ThreadId)
-                //    continue;
-
-                IntPtr pOpenThread = WinApi.OpenThread(ThreadAccess.SUSPEND_RESUME, false, pT.Id);
-                if (pOpenThread == IntPtr.Zero)
-                    continue;
-
-                WinApi.SuspendThread(pOpenThread);
-                WinApi.CloseHandle(pOpenThread);
-            }
+            pd.MemoryHandler.SuspendAllThreads();
         }
 
         private void ResumeProcess()
         {
-            foreach (ProcessThread pT in process.Threads)
-            {
-                IntPtr pOpenThread = WinApi.OpenThread(ThreadAccess.SUSPEND_RESUME, false, pT.Id);
-                if (pOpenThread == IntPtr.Zero)
-                    continue;
-
-                var suspendCount = 0;
-                do
-                {
-                    suspendCount = WinApi.ResumeThread(pOpenThread);
-                }
-                while (suspendCount > 0);
-
-                WinApi.CloseHandle(pOpenThread);
-            }
+            pd.MemoryHandler.ResumeAllThreads();
         }
     }
 }
